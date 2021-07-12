0bc88b7b8a969ee64058ba7580d361595c734efe
Nuwan Sanjeewa Abeysiriwardana <nuwansa@users.noreply.github.com>
2021-07-13 02:30:59 +0530

    pkc param support added

--- outstack-spring/src/main/java/com/cloudimpl/outstack/spring/security/Auth2Util.java
+++ outstack-spring/src/main/java/com/cloudimpl/outstack/spring/security/Auth2Util.java
@@ -59,8 +59,11 @@ public class Auth2Util {
         String tenantId = req.getHeaders().getFirst("X-TenantId");
         String userAgent = req.getHeaders().getFirst("User-Agent");
         String userData = req.getHeaders().getFirst("X-UserData");
+        String state = req.getHeaders().getFirst("state");
+        String codeChallenge = req.getHeaders().getFirst("code_challenge");
+        String codeChallengeMethod = req.getHeaders().getFirst("code_challenge_method");
         String remoteIp = req.getRemoteAddress().toString();
-        return new AuthenticationMeta.ClientMeta(clientId, clientSecret, code, redirectUri, codeVerfier, tenantId,accessType,userAgent,remoteIp,userData);
+        return new AuthenticationMeta.ClientMeta(clientId, clientSecret, code, redirectUri, codeVerfier, tenantId,accessType,userAgent,remoteIp,userData,state,codeChallenge,codeChallengeMethod);
     }
 
     public static void validateAuthentcationMeta(Object detail) {
