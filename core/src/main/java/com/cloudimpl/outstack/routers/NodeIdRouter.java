59cfc7fede2e9dee0e7a6e931c7c0eb2194dd84e
Nuwan Sanjeewa Abeysiriwardana <nuwansa@users.noreply.github.com>
2021-09-21 21:38:31 +0530

    fix retry bug

--- core/src/main/java/com/cloudimpl/outstack/routers/NodeIdRouter.java
+++ core/src/main/java/com/cloudimpl/outstack/routers/NodeIdRouter.java
@@ -32,7 +32,6 @@ import reactor.core.publisher.Mono;
  *
  * @author nuwansa
  */
-
 public class NodeIdRouter implements CloudRouter {
 
     private final Map<String, CloudService> nodeIdTopics = new ConcurrentHashMap<>();
@@ -41,30 +40,32 @@ public class NodeIdRouter implements CloudRouter {
     @Inject
     public NodeIdRouter(@Named("@topic") String topic, ServiceRegistryReadOnly serviceRegistry) {
         this.topic = topic;
-        serviceRegistry.services().filter(e -> e.name().equals(topic)).forEach(s->nodeIdTopics.put(s.nodeId(), s));
-        serviceRegistry.flux("NodeIdRouter:"+topic+":1").filter(e -> e.getType() == FluxMap.Event.Type.ADD || e.getType() == FluxMap.Event.Type.UPDATE)
+        serviceRegistry.services().filter(e -> e.name().equals(topic)).forEach(s -> nodeIdTopics.put(s.nodeId(), s));
+        serviceRegistry.flux("NodeIdRouter:" + topic + ":1").filter(e -> e.getType() == FluxMap.Event.Type.ADD || e.getType() == FluxMap.Event.Type.UPDATE)
                 .map(e -> e.getValue()).filter(e -> e.name().equals(topic))
-                .doOnNext(e -> System.out.println(this+"added node id router " + e))
+                .doOnNext(e -> System.out.println(this + "added node id router " + e))
                 .doOnNext(e -> nodeIdTopics.put(e.nodeId(), e)).subscribe();
-        serviceRegistry.flux("NodeIdRouter:"+topic+":2").filter(e -> e.getType() == FluxMap.Event.Type.REMOVE)
+        serviceRegistry.flux("NodeIdRouter:" + topic + ":2").filter(e -> e.getType() == FluxMap.Event.Type.REMOVE)
                 .map(e -> e.getValue()).filter(e -> e.name().equals(topic))
-                 .doOnNext(e -> System.out.println(this+"removed node id router " + e))
+                .doOnNext(e -> System.out.println(this + "removed node id router " + e))
                 .doOnNext(e -> nodeIdTopics.remove(e.nodeId())).subscribe();
     }
 
     @Override
     public Mono<CloudService> route(CloudMessage msg) {
-        CloudService srv = nodeIdTopics.get(msg.getKey());
-        if (srv != null) {
-            return Mono.just(srv);
-        } else {
-            System.out.println("testing" + nodeIdTopics.size() + " Key value "+ msg.getKey());
-            StringBuilder builder = new StringBuilder();
-            builder.append("service not found to route for topic [" + topic + "]");
-            
-            nodeIdTopics.entrySet().forEach(s -> builder.append(s));
-            return Mono.error(new RouterException(builder.toString()));
-        }
+        return Mono.defer(() -> {
+            CloudService srv = nodeIdTopics.get(msg.getKey());
+            if (srv != null) {
+                return Mono.just(srv);
+            } else {
+                System.out.println("testing" + nodeIdTopics.size() + " Key value " + msg.getKey());
+                StringBuilder builder = new StringBuilder();
+                builder.append("service not found to route for topic [" + topic + "]");
+
+                nodeIdTopics.entrySet().forEach(s -> builder.append(s));
+                return Mono.error(new RouterException(builder.toString()));
+            }
+        });
     }
 
 }
