939779d69ae738f472e94739bb345dcebadb62c4
Nuwan Sanjeewa Abeysiriwardana <nuwansa@users.noreply.github.com>
2022-02-21 18:17:32 +0530

    Update Cluster.java

--- outstack-spring/src/main/java/com/cloudimpl/outstack/spring/component/Cluster.java
+++ outstack-spring/src/main/java/com/cloudimpl/outstack/spring/component/Cluster.java
@@ -152,7 +152,7 @@ public class Cluster {
 
     public static Cluster getInstance()
     {
-        return this.instance;
+        return Cluster.instance;
     }
     
     public <T> Mono<T> requestReply(ServerHttpRequest httpRequest, String serviceName, Object msg) {
