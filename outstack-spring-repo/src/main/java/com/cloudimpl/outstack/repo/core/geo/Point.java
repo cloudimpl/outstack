1ff6f81cb9594f51bb0ef07f9ae2941bb4e357ae
Chandika Malimage <tmalimage@gmail.com>
2022-06-09 16:27:48 +0530

    no arg constructor

--- outstack-spring-repo/src/main/java/com/cloudimpl/outstack/repo/core/geo/Point.java
+++ outstack-spring-repo/src/main/java/com/cloudimpl/outstack/repo/core/geo/Point.java
@@ -2,9 +2,11 @@ package com.cloudimpl.outstack.repo.core.geo;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Point implements GeoMetry{
     private double lat;
     private double lon;
