88b81ae1d857ef3b8f7476b9e59d9ed669bb3efa
Yasas Ranawaka <ranawaka.y@gmail.com>
2022-09-21 22:36:37 +0530

    update-policy-rename

--- outstack-spring/src/main/java/com/cloudimpl/outstack/spring/service/iam/UpdatePolicy.java
+++ outstack-spring/src/main/java/com/cloudimpl/outstack/spring/service/iam/UpdatePolicy.java
@@ -4,6 +4,7 @@ import com.cloudimpl.outstack.core.Inject;
 import com.cloudimpl.outstack.runtime.EntityCommandHandler;
 import com.cloudimpl.outstack.runtime.EntityContext;
 import com.cloudimpl.outstack.runtime.ResourceHelper;
+import com.cloudimpl.outstack.runtime.ValidationErrorException;
 import com.cloudimpl.outstack.runtime.domain.Policy;
 import com.cloudimpl.outstack.runtime.domain.PolicyCreateRequest;
 import com.cloudimpl.outstack.runtime.domain.PolicyUpdated;
@@ -15,7 +16,11 @@ public class UpdatePolicy extends EntityCommandHandler<Policy, PolicyCreateReque
 
     @Override
     protected Policy execute(EntityContext<Policy> context, PolicyCreateRequest command) {
-        return context.update(command.getPolicyName(), new PolicyUpdated(command.getPolicyName(), command.getPolicyContext(), helper.getDomainOwner(), helper.getDomainContext(), helper.getApiContext(), command.getBoundary(), command.getDependentPolicies()));
+        Policy entity = context.<Policy>asRootContext().getEntity().orElseThrow(()->  new ValidationErrorException("Policy does not exists"));
+        if (!command.getPolicyName().equals(entity.getPolicyName())) {
+            context.rename(entity.getPolicyName(), command.getPolicyName());
+        }
+        return context.update(entity.id(), new PolicyUpdated(command.getPolicyName(), command.getPolicyContext(), helper.getDomainOwner(), helper.getDomainContext(), helper.getApiContext(), command.getBoundary(), command.getDependentPolicies()));
     }
 
 }
